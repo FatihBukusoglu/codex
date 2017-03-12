d30b967675e0232690649fcfa19cb022765d6164
FatihBukusoglu <fatihbukusoglu96@gmail.com>
2017-03-12 21:25:56 +0300

    Popup
    Çato teli kapat :octocat:

--- app/src/main/java/foreign_number/codex/MainMenu.java
+++ app/src/main/java/foreign_number/codex/MainMenu.java
@@ -2,10 +2,18 @@ package foreign_number.codex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainMenu extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
+
+    Button languageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,22 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-    }
+       languageButton = (Button)findViewById(R.id.language_button);
+       languageButton.setOnClickListener(new OnClickListener() {
 
+           @Override
+           public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainMenu.this,languageButton);
+                popup.getMenuInflater().inflate(R.menu.popup,popup.getMenu());
+
+               popup.setOnMenuItemClickListener(new  PopupMenu.OnMenuItemClickListener(){
+                   public boolean onMenuItemClick(MenuItem item){
+                       Toast.makeText(MainMenu.this,"Search Results For: " +item.getTitle(),Toast.LENGTH_SHORT).show();
+                       return true;
+                   }
+               });
+               popup.show();
+           }
+       });
+    }
 };
