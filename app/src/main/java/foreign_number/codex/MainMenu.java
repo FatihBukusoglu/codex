63a4694c1105fc634c4194c90f093a48d62ee8f5
FatihBukusoglu <fatihbukusoglu96@gmail.com>
2017-04-08 14:10:04 +0300

    a

--- app/src/main/java/foreign_number/codex/MainMenu.java
+++ app/src/main/java/foreign_number/codex/MainMenu.java
@@ -9,11 +9,31 @@ import android.widget.Button;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainMenu extends AppCompatActivity {
     private FirebaseAnalytics mFirebaseAnalytics;
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
     Button languageButton;
+
+    myRef.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+            String value = dataSnapshot.getValue(String.class);
+            Log.d(TAG, "Value is: " + value);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError error) {
+            // Failed to read value
+            Log.w(TAG, "Failed to read value.", error.toException());
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +41,10 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
+
+
        languageButton = (Button)findViewById(R.id.language_button);
+
        languageButton.setOnClickListener(new OnClickListener() {
 
            @Override
