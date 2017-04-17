15e845054229be92c621a8eade791138e46e08e9
FatihBukusoglu <fatihbukusoglu96@gmail.com>
2017-04-17 23:10:33 +0300

    Firebase bağlamtısı
    Çalışıyor aq :octocat:

--- app/src/main/java/foreign_number/codex/MainMenu.java
+++ app/src/main/java/foreign_number/codex/MainMenu.java
@@ -7,44 +7,56 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import java.lang.String;
 
-public class MainMenu extends AppCompatActivity {
-    private FirebaseAnalytics mFirebaseAnalytics;
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+public class MainMenu extends AppCompatActivity{
+
+    SearchView mSearchBar;
+    TextView mDiscriptionView;
+    Button mSearchButton;
     Button languageButton;
+    String choosen_Word = " ",choosen_Language = " ";
 
-    myRef.addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            // This method is called once with the initial value and again
-            // whenever data at this location is updated.
-            String value = dataSnapshot.getValue(String.class);
-            Log.d(TAG, "Value is: " + value);
-        }
-
-        @Override
-        public void onCancelled(DatabaseError error) {
-            // Failed to read value
-            Log.w(TAG, "Failed to read value.", error.toException());
-        }
-    });
+    //Get Database Referance
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        //Get UI elements
+        mDiscriptionView = (TextView) findViewById(R.id.result);
+        languageButton = (Button)findViewById(R.id.language_button);
+        mSearchButton = (Button) findViewById(R.id.search_button);
+        mSearchBar = (SearchView) findViewById(R.id.searchView);
 
 
+        //Search Bar
 
-       languageButton = (Button)findViewById(R.id.language_button);
+        mSearchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                choosen_Word = query.toString();
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                choosen_Word = newText.toString();
+                return false;
+            }
+        });
+
+        //Language Menu
        languageButton.setOnClickListener(new OnClickListener() {
 
            @Override
@@ -53,7 +65,10 @@ public class MainMenu extends AppCompatActivity {
                 popup.getMenuInflater().inflate(R.menu.popup,popup.getMenu());
 
                popup.setOnMenuItemClickListener(new  PopupMenu.OnMenuItemClickListener(){
+
                    public boolean onMenuItemClick(MenuItem item){
+                       choosen_Language = item.getTitle().toString();
+                       languageButton.setText(item.getTitle().toString());
                        Toast.makeText(MainMenu.this,"Search Results For: " +item.getTitle(),Toast.LENGTH_SHORT).show();
                        return true;
                    }
@@ -61,5 +76,30 @@ public class MainMenu extends AppCompatActivity {
                popup.show();
            }
        });
+
+        //Search Button
+        mSearchButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DatabaseReference mLeafRef = mRootRef.child(choosen_Language);
+                DatabaseReference mLeaf2 = mLeafRef.child(choosen_Word);
+
+                mLeaf2.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String text = dataSnapshot.getValue(String.class);
+                        mDiscriptionView.setText(text);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        String text = "Cannot found the search";
+                        mDiscriptionView.setText(text);
+                    }
+                });
+            }
+        });
     }
-};
+
+}
